9bef3264022a7e3aabffa8befbb24d84bda5897c
Dawid Jkw <djankowski@software.altkom.pl>
2016-08-04 11:35:55 +0200

    Buttons + comments changes

--- js/js.js
+++ js/js.js
@@ -1,5 +1,5 @@
 //variables to hold data
-var comment = ["Good job, keep it low!!!","Sugar monster. STOP!!!"];
+var comment = ["Good job, keep it low!!!","Sugar monster. STOP!!!","Sugar monster is up. Hide your cookies!", "Yeah, you wish!"];
 var score= [];
 
 
@@ -7,10 +7,14 @@ function get(){
 	if(localStorage.getItem("points") !==null){
 		score.push(parseInt(localStorage.getItem("points")));
 		document.getElementById("points").innerHTML = score.reduce((a, b) => a + b, 0)+"g";
-		if (score.reduce((a, b) => a + b, 0) > 30){
+		if (score.reduce((a, b) => a + b, 0) > 50){
 			document.getElementById("comment").innerHTML = comment[1];
-		} else {
+		} else if (score.reduce((a, b) => a + b, 0) <= 50 && score.reduce((a, b) => a + b, 0) >= 30) {
+			document.getElementById("comment").innerHTML = comment[2];
+		} else if (score.reduce((a, b) => a + b, 0) >= 0 && score.reduce((a, b) => a + b, 0) < 30) {
 			document.getElementById("comment").innerHTML = comment[0];
+		} else {
+			document.getElementById("comment").innerHTML = comment[3];
 		}
 	} else {
 		document.getElementById("points").innerHTML = 0+"g";
@@ -20,17 +24,26 @@ function get(){
 function storeSugar(){
 	//adds new question value from the input field to qAccepted array
 	if(document.getElementById("question").value != ''){
-		//adds new score to score array
-		score.push(parseInt(document.getElementById("question").value));
-		//prints sum of scores from array score
-		document.getElementById("points").innerHTML = score.reduce((a, b) => a + b, 0)+"g";
-		//evaluates total score so far and prints comment
-		if (score.reduce((a, b) => a + b, 0) > 30){
-			document.getElementById("comment").innerHTML = comment[1];
-			save();
+		if(document.getElementById("question").value < 0){
+			document.getElementById("comment").innerHTML = comment[3];
 		} else {
-			document.getElementById("comment").innerHTML = comment[0];
-			save();
+			//adds new score to score array
+			score.push(parseInt(document.getElementById("question").value));
+			//prints sum of scores from array score
+			document.getElementById("points").innerHTML = score.reduce((a, b) => a + b, 0)+"g";
+			//evaluates total score so far and prints comment
+			if (score.reduce((a, b) => a + b, 0) > 50){
+				document.getElementById("comment").innerHTML = comment[1];
+				save();
+			} else if (score.reduce((a, b) => a + b, 0) <= 50 && score.reduce((a, b) => a + b, 0) >= 30) {
+				document.getElementById("comment").innerHTML = comment[2];
+				save();
+			} else if (score.reduce((a, b) => a + b, 0) >= 0 && score.reduce((a, b) => a + b, 0) < 30) {
+				document.getElementById("comment").innerHTML = comment[0];
+				save();
+			} else {
+			document.getElementById("comment").innerHTML = comment[3];
+			}
 		}
 	} 
 }
