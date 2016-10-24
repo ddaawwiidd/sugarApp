b4be12e0c9d83d03f651daa177a570041f1122ff
Dawid Jkw <djankowski@software.altkom.pl>
2016-10-24 14:05:58 +0200

    sw_04
    first test

--- sw.js
+++ sw.js
@@ -1,2 +1,29 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('sugarApp').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/css/my.css',
+        '/css/normalize.css',
+        '/css/skeleton.css',
+        '/js/js.js',
+        '/images/sugar.png'
+      ])
+      .then(() => self.skipWaiting());
+    })
+  )
+});
 
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
 
